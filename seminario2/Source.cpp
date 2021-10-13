0a6eff14a73ccdadf6cacd6c9288095356a3f49e
Rubén Domenech Mora <rudomo@upv.edu.es>
2021-10-13 12:33:27 +0200

    seminario2

---
+++ seminario2/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <gl\freeglut.h>
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutWireTeapot(0.5);
+	glFlush();
+}
+void reshape(GLint w, GLint h)
+{
+}
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(200, 200);
+	glutCreateWindow("Hola Mundo");
+	std::cout << "Hola Mundo running" << std::endl;
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+}
